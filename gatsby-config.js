f43f874403e948e08001891ae0688d92571d7288
pithesun <wonjh0730@naver.com>
2024-05-27 00:53:38 +0900

    gatsbyjs 튜토리얼 4~7 완료

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@ module.exports = {
     title: `sunny's blog`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,39 @@
 import * as React from "react";
-import { Link } from "gatsby";
+import { Link, useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
-          <li className={navLinkText}>
+          <li className={navLinkItem}>
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className={navLinkItem}>
             <Link to="/about">About</Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
+            </Link>
           </li>
         </ul>
       </nav>
