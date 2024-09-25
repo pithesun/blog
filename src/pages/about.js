13b088bf6a49cf7a88bedd633eedafa0222f53d8
pithesun <wonjh0730@naver.com>
2024-09-26 00:22:18 +0900

    [글쓰기] About me

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,6 +26,15 @@ const AboutPage = () => {
           <col />
         </colgroup>
         <tr>
+          <th>오픈소스 컨트리뷰션 아카데미 수료</th>
+          <td> Git 활용 및 DevOps / MLOps 체험형 수료 2023.7~2023.8</td>
+          <td> Githru 오픈소스 참여형 2024.7 ~</td>
+        </tr>
+        <tr>
+          <th>현대아이티앤이 채용확정형 2021.7~2021.12</th>
+          <td>웹 개발 전반(vue, spring framework, oracle/java)</td>
+        </tr>
+        <tr>
           <th>중앙대학교 2017.3~2022.2</th>
           <td>
             국어국문학과 졸업 <br /> 컴퓨터공학과 복수전공
